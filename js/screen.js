d31411514c12ab2e22239610d19f188a4ef46c28
kenjos75 <kenjos75@yahoo.com>
2016-05-27 05:51:06 -0400

    Responsible for all javascript game libraries

---
+++ js/screen.js
@@ -0,0 +1,18 @@
+var GameScreen = function() {
+	
+
+	this.init = function() {
+
+		return {
+			width: 100,
+			height: 100
+		};
+
+
+	}
+	this.render = function() {
+
+		console.log('render');
+	}
+
+}
